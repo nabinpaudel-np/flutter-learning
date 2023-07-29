406bd188042dd75239b00a9b5e02261be8dc3b7c
nabinpaudel-np <linktonabin@gmail.com>
2023-07-29 13:38:35 +0545

    string and bool inn dart

---
+++ main2.dart
@@ -0,0 +1,33 @@
+//string
+//boolean
+
+void main(){
+
+//boolean : used to denote either true or false, boolean has only two value, true and false
+// we use "bool" to denote boolean in dart
+// true, false will be result of boolean operation
+// boolean => true, false
+// if we need to denote anything as true or false, we use boolean.
+
+
+
+int num = 11;
+bool isGreaterThan0 = num > 3;
+bool isLessThan0 = num < 2;
+
+print (isGreaterThan0);
+print(isLessThan0);
+
+// we use a lot of conditional operators in boolean
+// conditional operators:
+// <, >, <= , >= , == , != , !
+// == checks if two number is same or not.
+// 11 == 11 output is true
+// != checks if other number is not equal
+// 1 != 2 output is false
+
+
+
+}
+
+
