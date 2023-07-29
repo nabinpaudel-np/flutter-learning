e60e367787bfa5ec50c54525af4ba1333d499e1b
nabinpaudel-np <linktonabin@gmail.com>
2023-07-29 13:37:48 +0545

    data types in dart

---
+++ main.dart
@@ -0,0 +1,8 @@
+// day 1 of learning
+
+void main(){
+
+    String day1 = "Learning Flutter";
+    print (day1);
+
+}
--- main1.dart
+++ main1.dart
@@ -1,8 +1,39 @@
-// day 1 of learning
+//learning from day 1
 
 void main(){
 
-    String day1 = "Learning Flutter";
-    print (day1);
+    //data types
+    // int, double int
+
+    // int number = 1;
+    // double secondNumber = 2.10;
+    // String name = "Nabin Paudel";
+
+    // print(number);
+    // print(secondNumber);
+    // print(name);
+
+    int firstNumber = 1;
+    int secondNumber = 2;
+    int sum = firstNumber + secondNumber;
+    print(sum);
+    int difference = firstNumber - secondNumber;
+    print(difference);
+    int multiplication = firstNumber * secondNumber;
+    print(multiplication);
+    double division = firstNumber/secondNumber;
+    print (division);
+    int modulus = firstNumber % secondNumber;
+    print (modulus);
+
+    // increament ++
+    firstNumber++;
+    ++firstNumber;
+    // firstNumber = firstNumber + 1;
+    print(firstNumber);
+
+    //decreament
+    secondNumber = secondNumber - 1;
+    print(secondNumber);
 
 }
